29f0e66d08ec5f5d6a5c89b43954662fded0c977
aeyeopsdev <aeyeopsdev@aeyeops.com>
2026-04-23 07:39:18 +0000

    feat(contacts): add directory and other-contacts search tools

--- auth/service_decorator.py
+++ auth/service_decorator.py
@@ -60,7 +60,9 @@ from auth.scopes import (
     TASKS_SCOPE,
     TASKS_READONLY_SCOPE,
     CONTACTS_SCOPE,
+    CONTACTS_OTHER_READONLY_SCOPE,
     CONTACTS_READONLY_SCOPE,
+    DIRECTORY_READONLY_SCOPE,
     CUSTOM_SEARCH_SCOPE,
     SCRIPT_PROJECTS_SCOPE,
     SCRIPT_PROJECTS_READONLY_SCOPE,
@@ -557,6 +559,8 @@ SCOPE_GROUPS = {
     # Contacts scopes
     "contacts": CONTACTS_SCOPE,
     "contacts_read": CONTACTS_READONLY_SCOPE,
+    "contacts_other_read": CONTACTS_OTHER_READONLY_SCOPE,
+    "directory_read": DIRECTORY_READONLY_SCOPE,
     # Custom Search scope
     "customsearch": CUSTOM_SEARCH_SCOPE,
     # Apps Script scopes
